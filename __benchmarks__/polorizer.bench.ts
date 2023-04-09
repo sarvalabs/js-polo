0120a1033061d21f19f260e51297ccfb9b079c65
Gokul R <gokul@sarva.ai>
2023-04-09 19:48:06 +0530

    chore: bench test linter warnings resolved

--- __benchmarks__/polorizer.bench.ts
+++ __benchmarks__/polorizer.bench.ts
@@ -4,82 +4,82 @@ import { Polorizer } from '../src/polorizer.js';
 import { documentEncode } from '../src/document.js';
 
 const mixedObject = {
-    a: "Sins & Virtues",
-    b: 567822,
-    c: ["pride", "greed", "lust", "gluttony", "envy", "wrath", "sloth"],
-    d: new Map([
-        ["bravery", "piety"],
-        ["friendship", "chastity"]
-    ]),
-    e: 45.23
+	a: 'Sins & Virtues',
+	b: 567822,
+	c: ['pride', 'greed', 'lust', 'gluttony', 'envy', 'wrath', 'sloth'],
+	d: new Map([
+		['bravery', 'piety'],
+		['friendship', 'chastity']
+	]),
+	e: 45.23
 };
 
 const schema = {
-    kind: 'struct',
-    fields: {
-        a: {
-            kind: 'string'
-        },
-        b: {
-            kind: 'integer'
-        },
-        c: {
-            kind: 'array',
-            fields: {
-                values: {
-                    kind: 'string'
-                }
-            }
-        },
-        d: {
-            kind: 'map',
-            fields: {
-                keys: {
-                    kind: 'string'
-                },
-                values: {
-                    kind: 'string'
-                }
-            }
-        },
-        e: {
-            kind: 'float'
-        }
-    }
+	kind: 'struct',
+	fields: {
+		a: {
+			kind: 'string'
+		},
+		b: {
+			kind: 'integer'
+		},
+		c: {
+			kind: 'array',
+			fields: {
+				values: {
+					kind: 'string'
+				}
+			}
+		},
+		d: {
+			kind: 'map',
+			fields: {
+				keys: {
+					kind: 'string'
+				},
+				values: {
+					kind: 'string'
+				}
+			}
+		},
+		e: {
+			kind: 'float'
+		}
+	}
 };
 
-(() => {  
-    const suite = new Benchmark.Suite;  
-    const polorizer = new Polorizer();
-    polorizer.polorize(mixedObject, schema)
-    const wire = polorizer.bytes();
+(():void => {  
+	const suite = new Benchmark.Suite;  
+	const polorizer = new Polorizer();
+	polorizer.polorize(mixedObject, schema);
+	const wire = polorizer.bytes();
     
     
-    suite.add("Polorize", () => {
-        const polorizer = new Polorizer();
-        polorizer.polorize(mixedObject, schema);
-    }).add("Depolorize", () => {
-        const depolorizer = new Depolorizer(wire);
-        depolorizer.depolorize(schema);
-    }).on("cycle", (event) => {
-        console.log(String(event.target));
-    }).run({ 'async': true });
+	suite.add('Polorize', () => {
+		const polorizer = new Polorizer();
+		polorizer.polorize(mixedObject, schema);
+	}).add('Depolorize', () => {
+		const depolorizer = new Depolorizer(wire);
+		depolorizer.depolorize(schema);
+	}).on('cycle', (event) => {
+		console.log(String(event.target));
+	}).run({ 'async': true });
 })();
 
-(() => {
-    const suite = new Benchmark.Suite;
-    const doc = documentEncode(mixedObject, schema);
-    const docWire = doc.bytes();
+(():void => {
+	const suite = new Benchmark.Suite;
+	const doc = documentEncode(mixedObject, schema);
+	const docWire = doc.bytes();
 
-    suite.add("Document Encode", () => {
-        documentEncode(mixedObject, schema);
-    }).add("Decode To Document", () => {
-        const deplorizer = new Depolorizer(docWire)
-        deplorizer.depolorizeDocument()
-    }).add("Decode To Struct", () => {
-        const deplorizer = new Depolorizer(docWire)
-        deplorizer.depolorize(schema)
-    }).on("cycle", (event) => {
-        console.log(String(event.target));
-    }).run({ 'async': true });
+	suite.add('Document Encode', () => {
+		documentEncode(mixedObject, schema);
+	}).add('Decode To Document', () => {
+		const deplorizer = new Depolorizer(docWire);
+		deplorizer.depolorizeDocument();
+	}).add('Decode To Struct', () => {
+		const deplorizer = new Depolorizer(docWire);
+		deplorizer.depolorize(schema);
+	}).on('cycle', (event) => {
+		console.log(String(event.target));
+	}).run({ 'async': true });
 })();
