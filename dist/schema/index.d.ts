5ec2a9ecf71f4c6e37c4c10cb1b09d31692c4d00
Harsh Rastogi <rastogiharsh04@gmail.com>
2025-03-13 14:37:01 +0530

    chore: push build

--- dist/schema/index.d.ts
+++ dist/schema/index.d.ts
@@ -1,3 +1,13 @@
+/*!
+ * @license MIT
+ * @author 0xharshrastogi
+ * @repository https://github.com/0xharshrastogi/polo-schema
+ *
+ * This idea has been adapted from the following repository:
+ *
+ * The original repository's license can be found here:
+ * https://github.com/0xharshrastogi/polo-schema/blob/main/LICENSE
+ */
 export { schema } from './schema';
 export { type SchemaDefinition } from './schema-definition';
 export { type ArraySchema, type MapSchema, type PrimitiveKind, type PrimitiveSchema, type Schema, type StructPropertyKey, type StructSchema } from './schema-types';
--- dist/schema/index.js
+++ dist/schema/index.js
@@ -1,5 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.schema = void 0;
+/*!
+ * @license MIT
+ * @author 0xharshrastogi
+ * @repository https://github.com/0xharshrastogi/polo-schema
+ *
+ * This idea has been adapted from the following repository:
+ *
+ * The original repository's license can be found here:
+ * https://github.com/0xharshrastogi/polo-schema/blob/main/LICENSE
+ */
 var schema_1 = require("./schema");
 Object.defineProperty(exports, "schema", { enumerable: true, get: function () { return schema_1.schema; } });
